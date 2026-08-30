5db23f6e97b44e7ec29d132ad581bc28e8c81a68
Sarvadnya Yeole <yeolesv1012@gmail.com>
2026-08-30 21:59:38 +0530

    Did basic setup for ML project

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup  # type: ignore[import-not-found]
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(path: str) -> List[str]:
+    with open(path) as fs:
+        requirements = fs.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Sarvadnya",
+    author_email="your-email@example.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=get_requirements("requirements.txt"),
+)
